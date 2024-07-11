ba75a0832612df7503926d9dfbf21191e933d224
OfekMajar <ofek.majar@gmail.com>
2024-07-11 22:13:00 +0300

    [HOS-22] added fullName function

--- models/user.model.js
+++ models/user.model.js
@@ -4,8 +4,16 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String },
   email: { type: String, required: true, unique: true },
-  profilePicture: { type: String }, // Add this field for profile picture URL
+  profilePicture: { type: String },
 });
 
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+userSchema.set("toJSON", { virtuals: true });
+userSchema.set("toObject", { virtuals: true });
+
 const User = mongoose.model("User", userSchema);
+
 module.exports = { User };
